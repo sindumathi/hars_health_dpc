7c2f5b66a3e23c9da645e011657757fafce41e8e
sindumathi <kmsindumathi@gmail.com>
2026-08-11 23:13:34 -0500

    Bugfix: redirect to login

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: false, // set to true if it's a permanent 308 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
